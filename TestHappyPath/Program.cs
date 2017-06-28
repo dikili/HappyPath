f2ea0e52077934de6d85de0c597785e6cea9328a
ICENI\id1 <ismail.dikili@redcloudtechnology.com>
2017-06-28 17:52:30 +0100

    happy path added...

---
+++ TestHappyPath/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestHappyPath
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is a happy path to demostrate the happy path");
+
+            Console.ReadLine();
+        }
+    }
+}
